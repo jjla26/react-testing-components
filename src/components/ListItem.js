d342eac64814de0ab3a3185c8150a6b28009c306
Julio Lopez <julioj.lopeza@gmail.com>
2022-08-15 21:10:38 +0200

    refactor listitem

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import styles from '../styles/ListItem.module.css'
 
-function ListItem({ item, selected, pointer, onClickCallback }) {
+function ListItem({ children, selected, pointer, onClickCallback }) {
   const pointerClass = pointer ? 'pointer' : ''
   const selectedClass = selected ? 'selected' : ''
 
   return (
     <li
-      className={`${styles.listItem} ${styles[selectedClass]} ${styles[pointerClass]}`} 
-      onClick={() => onClickCallback(item)}>
-        {item}
+      className={`${styles.listItem} ${styles[selectedClass]} ${styles[pointerClass]}`}
+      onClick={() => onClickCallback && onClickCallback(children)}>
+        {children}
     </li>
   )
 }
