dab5699a3e65c5831705a5035995a434bf50c203
Julio Lopez <julioj.lopeza@gmail.com>
2022-08-15 20:39:34 +0200

    add screen functionality

--- src/App.js
+++ src/App.js
@@ -1,11 +1,86 @@
-import './App.css';
-import Input from './components/Input';
+import { useState } from 'react';
+import Card from './components/Card';
+import TeamsMembersForm from './components/TeamsMembersForm';
+import TeamsList from './components/TeamsList';
+import MembersList from './components/MembersList';
+
+import styles from './styles/App.module.css'
 
 function App() {
-  return (
-    <div className="App">
-      <Input />
+  const [ value, setValue ] = useState('')
+  const [ error, setError ] = useState('')
+  const [ mode, setMode ] = useState('member')
+  const [ teams, setTeams ] = useState([{id: 0, name: 'Team Orange', selected: true},{id:1, name: 'Team Yellow'},{id: 2, name: 'Team Purple'}])
+  const [ members, setMembers ] = useState([{id: 0, name: 'Julio', team: 0}, {id: 1, name: 'Adam', team: 0}, {id: 2, name: 'Thomas', team: 0}])
+  const selectedTeamId = teams.find(team => team.selected).id
 
+  const handleOnChange = (e) => {
+    if(e.target.type === 'radio'){
+      setMode(e.target.value)
+    }else{
+      setValue(e.target.value)
+    }
+  }
+
+  const handleSelect = name => {
+    setTeams(teams.map(team => {
+      return {...team, selected: team.name === name}
+    }))
+  }
+
+  const onRemoveCallback = name => {
+    setMembers(members.filter(member => member.name !== name))
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if(validate()){
+      if (mode === 'member'){
+        setMembers([...members, {id: members.length, name: value, team: selectedTeamId }])
+      }else {
+        setTeams([...teams, {id: teams.length, name: value }])
+      }
+      setValue('')
+    }
+  }
+
+  const validate = () => {
+    if(value === ''){
+      setError(`You can't add an empty ${mode}`)
+      return false
+    }
+    if(mode === 'team' && teams.find(member => member.name === value)) {
+      setError(`Team ${value} already exists`)
+      return false
+    }
+    if(mode === 'member' && members.filter(member => member.team === selectedTeamId).find(member => member.name === value)) {
+      setError(`Member ${value} already exist`)
+      return false
+    }
+    setError('')
+    return true
+  }
+
+  return (
+    <div className={styles.App}>
+      <h1>Sofatutor Teams App</h1>
+      <div className={styles.columnContainer}>
+        <Card>
+          <TeamsMembersForm handleSubmit={handleSubmit} handleOnChange={handleOnChange} value={value} error={error} mode={mode} />
+        </Card>
+        <div className={styles.rowContainer}>
+          <div className={styles.teamsContainer}>
+            <Card>
+              <TeamsList teams={teams} handleSelectCallback={handleSelect}/>
+            </Card>
+          </div>
+          <div className={styles.membersContainer}>
+            <Card>
+              <MembersList members={members} selectedTeamId={selectedTeamId} onRemoveCallback={onRemoveCallback}/>
+            </Card>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
