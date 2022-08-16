97309f1960e300e6cb1e20149e176376aee610f5
Julio Lopez <julioj.lopeza@gmail.com>
2022-08-16 11:20:40 +0200

    add space

--- src/components/RemovableListItem.js
+++ src/components/RemovableListItem.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ListItem from './ListItem'
 
 const RemovableListItem = ({ onRemoveCallback, children, ...rest }) => {
-  return <div style={{ display: 'flex', alignItems: 'center'}}>
+  return <div style={{ display: 'flex', alignItems: 'center' }}>
     <ListItem {...rest}>{children}</ListItem>
     <span onClick={() => onRemoveCallback(rest.item)}>X</span>
   </div>
