6dbb00f862afead5992cf0ef298060c17ae752ed
Julio Lopez <julioj.lopeza@gmail.com>
2022-08-17 09:48:11 +0200

    delete space

--- src/components/TeamsMembersForm.js
+++ src/components/TeamsMembersForm.js
@@ -8,7 +8,7 @@ const TeamsMembersForm = ({ handleSubmit, handleOnChange, value, error, mode }) 
       <Input type='text' onChange={handleOnChange} value={value} />
       <p style={{color: 'red'}}>{error}</p>
       <div style={{margin: '10px'}}>
-        <Input type='radio' radioLabel='team' value='team' checked={mode === 'team'} name='mode'onChange={handleOnChange}/>
+        <Input type='radio' radioLabel='team' value='team' checked={mode === 'team'} name='mode' onChange={handleOnChange}/>
         <Input type='radio' radioLabel='member' value='member' checked={mode === 'member'} name='mode' onChange={handleOnChange} />
       </div>
       <Button type='submit' name={mode === 'member' ? 'Add Member' : 'Add Team'}/>
